1ab62e74580990481aab9d4ce83e5d094fd7929f
CLaire Cullimore <cascullimore@gmail.com>
2021-02-24 18:39:10 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'projects#home'
+  get 'home', to: 'projects#home'
+  get 'work', to: 'portfolio#show'
 end
